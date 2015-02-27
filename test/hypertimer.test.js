e10d5a0a23fd464daac2d2f5cc23f0bbc160b907
jos <wjosdejong@gmail.com>
2015-02-27 10:10:36 +0100

    Use babel transpiler in mocha

--- test/hypertimer.test.js
+++ test/hypertimer.test.js
@@ -3,7 +3,7 @@
 var assert = require('assert');
 var async = require('async');
 var seed = require('seed-random');
-var hypertimer = require('../dist/hypertimer');
+var hypertimer = require('../index');
 
 // TODO: split hypertimer.test.js in separate files, it has grown too large
 
